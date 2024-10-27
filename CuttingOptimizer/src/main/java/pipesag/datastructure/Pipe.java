529939268d816e0df24955bc8f484fa001218e52
montahaee <morteza.montahaee@outlook.com>
2024-10-27 14:34:40 +0100

    Overridden the equals method for the Pipe Object.

--- CuttingOptimizer/src/main/java/pipesag/datastructure/Pipe.java
+++ CuttingOptimizer/src/main/java/pipesag/datastructure/Pipe.java
@@ -40,4 +40,38 @@ public class Pipe {
     public double getLength() {
         return length;
     }
+
+    /**
+     * Compares this Pipe object with another object for equality.
+     *<p>
+     * This method performs the following checks:
+     * <ol>
+     *   <li>If the provided object (`obj`) is the same reference as this Pipe instance,
+     *       it returns true immediately, as they are inherently equal.</li>
+     *   <li>If the provided object is an instance of Pipe (and not null), it proceeds
+     *       to compare their lengths. Equality is determined by checking if the `length`
+     *       of this Pipe is identical to the length of the other Pipe.</li>
+     *   <li>If the provided object is not a Pipe instance, it returns false.</li>
+     * </ol>
+     *
+     *<P>
+     * This implementation leverages Java's `instanceof` operator, which handles null
+     * checks automatically.
+     * If `obj` is null, `obj instanceof Pipe` will return false,
+     * allowing the method to return false without an explicit null check.
+     *
+     * @param obj the object to compare with this Pipe instance
+     * @return true if `obj` is a Pipe with the same `length` as this Pipe;
+     *         otherwise, false
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Pipe pipe) {
+            return length == pipe.getLength();
+        }
+        return false;
+    }
 }
